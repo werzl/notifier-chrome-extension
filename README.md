e0c620486fe0ea44bfc08d477feca8019e44e977
Adam Hewitt <ahewitt@glasswall.com>
2024-07-18 21:35:15 +0100

    working github actions status poller

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # notifier-chrome-extension
 Notify when pipelines / pull requests complete on popular cloud platforms.
+
+
+# Setup
+Generate PAT token with the following scopes:
+- workflow
+- repo
+- user
+- project
+- notification
+
+Then add the string to line 3 in `app.js`.
--- app.js
+++ app.js
@@ -1,45 +1,72 @@
-document.getElementById("retrieveWorkflowsButton").addEventListener("click", retrieveWorkflows);
+document
+    .getElementById("retrieveWorkflowsButton")
+    .addEventListener("click", () => pollForPipelineUpdate(''));
 
-async function getData() {
-    const url = "https://api.github.com/repos/werzl/notifier-chrome-extension/actions/runs";
-
+async function getData(url, personalAccessToken) {
     try {
-      const response = await fetch(url, { headers: { "Authorization": "Bearer " }});
+        const response = await fetch(url, {
+            headers: {
+                Authorization:
+                    `Bearer ${personalAccessToken}`,
+            },
+        });
 
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      return json;
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
 
-    } 
-    catch (error) {
-      console.error(error.message);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error.message);
     }
-  }
-
-async function retrieveWorkflows() {
-    console.log("retrieving workflows");
-    
-    let workflows = await getData();
-
-    var structured_workflows = JSON.stringify(workflows);
-
-    workflows.workflow_runs.forEach( e => {
-      var icon = '';
-
-      if (e.conculsion === 'undefined'){
-        icon = '<i class="fi fi-rs-lightbulb-exclamation"></i>'
-      }
-
-      document.getElementById("workflows-table").innerHTML = '<tr><td>' + e.name + '</td><td>' + e.conclusion + '</td></tr>'
-    })
-
-     = structured_workflows
-    
-
-
-    console.log("retrieved workflows");
 }
 
+async function retrieveWorkflows(fullName, personalAccessToken) {
+    console.log("retrieving workflows");
+
+    var workflowRuns = await getData(`https://api.github.com/repos/${fullName}/actions/runs?per_page=100`, personalAccessToken);
+
+    var latestWorkflowRunNumbers = workflowRuns.workflow_runs.map((workflow) => {
+        return workflow.run_number;
+    });
+
+    var latestRun = Math.max(...latestWorkflowRunNumbers);
+
+    var workflows = workflowRuns.workflow_runs.filter((workflow) => workflow.run_number === latestRun).map((workflow) => {
+        return {
+            name: workflow.repository.name,
+            repo: fullName,
+            status: workflow.status,
+            conclusion: workflow.conclusion
+        };
+    });
+
+    console.log("retrieved workflows");
+
+    return workflows;
+}
+
+async function retrieveRepos(personalAccessToken) {
+    console.log("retrieving repositories");
+
+    var repos = await getData("https://api.github.com/user/repos?per_page=100", personalAccessToken);
+
+    document.getElementById("workflows-table").innerHTML = "";
+    
+    await repos.forEach(async (repo) => {
+        workflows = await retrieveWorkflows(repo.full_name);
+
+        workflows.forEach((workflow) => {
+            document.getElementById("workflows-table").innerHTML += '<tr><td>' + workflow.name + '</td><td>' + workflow.status + '</td></tr>';
+        });
+    })
+
+    console.log("retrieved repositories");
+
+    setTimeout(async () => await retrieveRepos(personalAccessToken), 5000);
+}
+
+function pollForPipelineUpdate(personalAccessToken) {
+    setTimeout(async () => await retrieveRepos(personalAccessToken), 5000);
+}
