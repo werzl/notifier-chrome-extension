6ac0422d8b2057ce3e075979fc62eec4b74df96f
Tomas <tpilvelis@ymail.com>
2024-07-18 20:07:02 +0100

    uoda

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ async function getData() {
     const url = "https://api.github.com/repos/werzl/notifier-chrome-extension/actions/runs";
 
     try {
-      const response = await fetch(url, { headers: { "Authorization": "Bearer " }});
+      const response = await fetch(url, { headers: { "Authorization": "Bearer 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" }});
 
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
@@ -24,7 +24,21 @@ async function retrieveWorkflows() {
     
     let workflows = await getData();
 
-    document.getElementById("workflows").innerText = JSON.stringify(workflows);
+    var structured_workflows = JSON.stringify(workflows);
+
+    workflows.workflow_runs.forEach( e => {
+      var icon = '';
+
+      if (e.conculsion === 'undefined'){
+        icon = '<i class="fi fi-rs-lightbulb-exclamation"></i>'
+      }
+
+      document.getElementById("workflows-table").innerHTML = '<tr><td>' + e.name + '</td><td>' + e.conclusion + '</td></tr>'
+    })
+
+     = structured_workflows
+    
+
 
     console.log("retrieved workflows");
 }
